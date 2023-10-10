d768f58d78754459a239830200650a6bde84ec74
sttere <sttere@mail.ru>
2023-10-10 16:46:06 +0500

    HW 10102023

---
+++ Fraction/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        Fraction A = new Fraction();
+        A.print();
+        System.out.println(A.getDenominator());
+
+        Fraction B = new Fraction(5);
+        B.print();
+
+        Fraction C = new Fraction(1,2);
+        C.print();
+
+        Fraction D = new Fraction(2,3,4);
+        D.print();
+
+        Fraction E = new Fraction(D);
+        E.print();
+    }
+}
